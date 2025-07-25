306d48d788926df8e78f84e7341ea6535ba66def
abefan2019 <abefan2019@gmail.com>
2025-07-25 11:15:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-import os
 import anime1_in_search
 import anime1_in_download
 def main():
-    if (not os.path.exists("anime"))or(not os.path.isdir("anime")):
-        os.mkdir("anime")
     while True:
         name=input("name: ")
         if name:
-            url=anime1_in_search.main(name)
-            anime1_in_download.main(url)
+            season,url=anime1_in_search.main(name)
+            anime1_in_download.main(season,url)
         else:
             break
     print("Program finished")
