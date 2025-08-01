c53dae7adcb1b5776d5b734b36579c98306d74f2
abefan2019 <abefan2019@gmail.com>
2025-08-01 11:03:02 +0800

    Update regex.py

--- regex.py
+++ regex.py
@@ -1,4 +1,6 @@
 import re
+import tkinter as tk
+import graphics
 def find(code,s1,s2):
     return re.findall(s1+"(.*?)"+s2,code,re.DOTALL)
 def files(code,types):
@@ -17,3 +19,33 @@ def m3u8(murl,files):#remove ad
     else:
         uuid=int(files[-1][-9:-3])
         return [path(murl)+file for file in files if int(file[-9:-3])<=uuid]
+def inter(opitions):
+    if graphics.on:
+        return graphics.inter(opitions)
+    else:
+        for i in range(len(opitions)):
+            print("("+str(i+1)+")",opitions[i])
+        n=0
+        while True:
+            n=input("choose (None for all): ")
+            if n:
+                try:
+                    n=int(n)
+                    if not (n<1 or n>len(opitions)):
+                        return [opitions[n-1]]
+                except:
+                    pass
+                print("enter number between 1 and",len(opitions))
+            else:
+                return opitions
+def outer(*texts,**args):
+    string=""
+    for text in texts:
+        string+=str(text)+" "
+    if graphics.on:
+        graphics.outer(string,**args)
+    else:
+        if "end" in args:
+            print(string,end=args["end"])
+        else:
+            print(string)
